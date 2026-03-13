24e8368ebcb560803a9b6f216d3fdfca8071802b
yanis3562 <evanmike720@gmail.com>
2026-03-13 21:52:31 +0100

    Add files via upload

---
+++ Glock19Tactical/cl_weaponNames.lua
@@ -0,0 +1,5 @@
+AddTextEntry("WEAPON_FM1_GLOCK19", "Glock 19 Tactical")
+AddTextEntry("WEAPON_FM1_M9A3", "Beretta M9A3")
+AddTextEntry("WEAPON_FM1_CZ75", "CZ-75")
+AddTextEntry("WEAPON_FM1_P226", "Sig Sauer P226")
+AddTextEntry("WEAPON_FM1_P320", "Sig Sauer P320")
